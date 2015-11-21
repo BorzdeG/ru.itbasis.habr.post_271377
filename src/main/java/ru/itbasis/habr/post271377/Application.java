d27b53d7bd5295349e79d788b78c3fa4d930890f
Victor (Borz) Alenkov <Alenkov.Victor@gmail.com>
2015-11-21 23:49:12 +0300

    append java format

--- src/main/java/ru/itbasis/habr/post271377/Application.java
+++ src/main/java/ru/itbasis/habr/post271377/Application.java
@@ -12,18 +12,46 @@ import java.util.Locale;
 import static org.apache.poi.ss.util.CellUtil.getCell;
 
 public class Application {
+	public static final String BKT_JSON_L = "[";
+	public static final String BKT_JSON_R = "]";
+	public static final String BKT_JAVA_L = "{";
+	public static final String BKT_JAVA_R = "}";
+	public static final String COMMA      = ",";
+
+	private static String BKT_START;
+	private static String BKT_END;
+	private static String BKT_I_SEPARATOR;
+	private static String BKT_J_SEPARATOR;
+
+	private static void initBrackets(final String bracketType) {
+		if (bracketType != null && !bracketType.isEmpty()) {
+			BKT_START = BKT_JAVA_L + BKT_JAVA_L + BKT_JAVA_L;
+			BKT_END = BKT_JAVA_R + BKT_JAVA_R + BKT_JAVA_R;
+			BKT_I_SEPARATOR = BKT_JAVA_R + BKT_JAVA_R + COMMA + BKT_JAVA_L + BKT_JAVA_L;
+			BKT_J_SEPARATOR = BKT_JAVA_R + COMMA + BKT_JAVA_L;
+
+		} else {
+			BKT_START = BKT_JSON_L + BKT_JSON_L + BKT_JSON_L;
+			BKT_END = BKT_JSON_R + BKT_JSON_R + BKT_JSON_R;
+			BKT_I_SEPARATOR = BKT_JSON_R + BKT_JSON_R + COMMA + BKT_JSON_L + BKT_JSON_L;
+			BKT_J_SEPARATOR = BKT_JSON_R + COMMA + BKT_JSON_L;
+		}
+	}
+
 	public static void main(String[] args) throws Exception {
 		final String filePath = args[0];
 		final File file = new File(filePath);
-		final String json = new Application().toJson(file);
-		System.out.println(json);
+
+		System.out.println((args.length > 1) ? toStringArray(file, args[1]) : toStringArray(file));
 	}
 
-	public String toJson(final File file) throws Exception {
+	public static String toStringArray(final File file, final String bracketType) throws Exception {
+		initBrackets(bracketType);
+
 		final Workbook wb = WorkbookFactory.create(file);
 		final Sheet sheet = wb.getSheetAt(0);
 
-		final StringBuilder sb = new StringBuilder("[[[");
+		final StringBuilder sb = new StringBuilder(BKT_START);
 
 		final int lastRowNum = sheet.getLastRowNum();
 		double i = 0, j = 0;
@@ -34,25 +62,29 @@ public class Application {
 			if (cellI.getCellType() != Cell.CELL_TYPE_NUMERIC) { break; }
 			final double tmpI = cellI.getNumericCellValue();
 			if (tmpI > i) {
-				if (i > 0) { sb.append("]],[["); }
+				if (i > 0) { sb.append(BKT_I_SEPARATOR); }
 				j = 0;
 			}
 
 			final double tmpJ = getCell(row, 1).getNumericCellValue();
 			if (tmpJ > j) {
-				if (j > 0) {sb.append("],[");}
+				if (j > 0) {sb.append(BKT_J_SEPARATOR);}
 			}
 
-			if (tmpJ == j) {sb.append(",");}
+			if (tmpJ == j) {sb.append(COMMA);}
 
 			// write value
 			sb.append(String.format(Locale.ENGLISH, "%.2f", getCell(row, 3).getNumericCellValue()));
 
-			// close tags
+			// next step
 			j = tmpJ;
 			i = tmpI;
 		}
 
-		return sb.append("]]]").toString();
+		return sb.append(BKT_END).toString();
+	}
+
+	public static String toStringArray(final File file) throws Exception {
+		return toStringArray(file, null);
 	}
 }
--- src/test/java/ru/itbasis/habr/post271377/ApplicationTest.java
+++ src/test/java/ru/itbasis/habr/post271377/ApplicationTest.java
@@ -12,27 +12,40 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.PrintStream;
 
-import static org.testng.Assert.*;
-
 public class ApplicationTest {
+
+	private static final String ROOT_PATH = "./src/test/resources/";
+
 	private ByteArrayOutputStream outContent;
 
 	private static String cleanString(final String str) {
 		return str.replaceAll("[\t|\n|\r| ]", "");
 	}
 
-	@DataProvider(name = "data")
-	public static Object[][] data() throws Exception {
-		final String rootPath = "./src/test/resources/";
-
-		final File expectFile = new File(rootPath, "expect0.json");
-		Assert.assertTrue(expectFile.exists());
-		final String expectJson = cleanString(IOUtils.toString(new FileInputStream(expectFile)));
+	@DataProvider(name = "dataJava")
+	public static Object[][] dataJava() throws Exception {
+		final String expectText = getExpectedText().replaceAll("\\[", "{").replaceAll("]", "}");
 
 		return new Object[][]{
-			{new File(rootPath, "sample0.xlsx"), expectJson},
-			{new File(rootPath, "sample0.xls"), expectJson}
+			{new File(ROOT_PATH, "sample0.xlsx"), expectText},
+			{new File(ROOT_PATH, "sample0.xls"), expectText}
 		};
+	}
+
+	@DataProvider(name = "dataJson")
+	public static Object[][] dataJson() throws Exception {
+		final String expectText = getExpectedText();
+
+		return new Object[][]{
+			{new File(ROOT_PATH, "sample0.xlsx"), expectText},
+			{new File(ROOT_PATH, "sample0.xls"), expectText}
+		};
+	}
+
+	private static String getExpectedText() throws Exception {
+		final File expectFile = new File(ROOT_PATH, "expect0.json");
+		Assert.assertTrue(expectFile.exists());
+		return cleanString(IOUtils.toString(new FileInputStream(expectFile)));
 	}
 
 	@BeforeMethod
@@ -46,17 +59,31 @@ public class ApplicationTest {
 		if (outContent != null) { outContent.close(); }
 	}
 
-	@Test(dataProvider = "data", dependsOnMethods = "testToJson")
-	public void testMain(final File file, final String expectedJson) throws Exception {
+	@Test(dataProvider = "dataJava", dependsOnMethods = "testJavaToStringArray", groups = "java")
+	public void testJavaMainToStringArray(final File file, final String expected) throws Exception {
+		Application.main(new String[]{file.getAbsolutePath(), "java"});
+		Assert.assertEquals(cleanString(outContent.toString()), expected);
+	}
+
+	@Test(dataProvider = "dataJava", groups = "java")
+	public void testJavaToStringArray(final File file, final String expectedJson) throws Exception {
+		Assert.assertTrue(file.exists());
+
+		final String json = Application.toStringArray(file, "java");
+		Assert.assertEquals(cleanString(json), expectedJson);
+	}
+
+	@Test(dataProvider = "dataJson", dependsOnMethods = "testJsonToStringArray", groups = "json")
+	public void testJsonMainToStringArray(final File file, final String expectedJson) throws Exception {
 		Application.main(new String[]{file.getAbsolutePath()});
 		Assert.assertEquals(cleanString(outContent.toString()), expectedJson);
 	}
 
-	@Test(dataProvider = "data")
-	public void testToJson(final File file, final String expectedJson) throws Exception {
+	@Test(dataProvider = "dataJson", groups = "json")
+	public void testJsonToStringArray(final File file, final String expectedJson) throws Exception {
 		Assert.assertTrue(file.exists());
 
-		final String json = new Application().toJson(file);
+		final String json = Application.toStringArray(file);
 		Assert.assertEquals(cleanString(json), expectedJson);
 	}
 }
